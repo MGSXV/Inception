7e3f5d4a717711dbe27a952a42912afda4ff9d91
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-06-21 15:00:56 +0100

    script

--- src/requirements/WordPress/tools/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-directory="/var/www/html/wordpress"
-if [ ! -d $directory ]
-then
-	curl -LO https://wordpress.org/latest.tar.gz
-	tar -xzf latest.tar.gz
-	
-fi
---
+++ src/requirements/WordPress/tools/wp_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = wordpress:9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
+chmod -R 775 /var/www/html/sel-kham;
+chown -R www-data /var/www/html/sel-kham;
+mkdir -p /run/php/;
+touch /run/php/php7.3-fpm.pid;
+
+
+if [ ! -f /var/www/html/sel-kham/wp-config.php ]; then
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
+chmod +x wp-cli.phar;
+mv wp-cli.phar /usr/local/bin/wp;
+cd /var/www/html/sel-kham;
+
+wp core download --allow-root;
+mv /var/www/wp-config.php /var/www/html/sel-kham;
+wp core install --allow-root --url=sel-kham.42.fr --title=${WORDPRESS_NAME} --admin_user=${WORDPRESS_ROOT_LOGIN} --admin_password=${DB_ROOT_PASSWORD} --admin_email=${WORDPRESS_ROOT_EMAIL};
+wp user create ${MYSQL_USER} ${WORDPRESS_USER_EMAIL} --user_pass=${MYSQL_PASSWORD} --role=author --allow-root;
+wp plugin update --all --allow-root
+else
+echo "Wordpress: is already set up!"
+fi
+/usr/sbin/php-fpm7.3 -F
