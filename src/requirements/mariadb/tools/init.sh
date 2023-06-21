8a1f2c21e95b61ad59d70ecb4ba39ce7318badae
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-06-21 01:37:11 +0100

    <note>: mariadb still exits w/ 1 err code

--- src/requirements/mariadb/tools/init.sh
+++ src/requirements/mariadb/tools/init.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-
+directory="/var/lib/mysql/wordpress"
 service mysql start
-chmod +x sql_secure_install.exp
-./sql_secure_install.exp
+if [ ! -d $directory ]
+then
+	chmod +x sql_secure_install.exp
+	./sql_secure_install.exp > /dev/null
+	mv /etc/mysql/mariadb.conf.d/50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf.default
+	mv 50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
+	rm -rf 50-server.cnf
+	service mysql reload
+fi
 exec "$@"
