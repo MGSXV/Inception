cc8e0a4a150581377621bf594ec520e91fbbb47a
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-06-21 12:57:21 +0100

    <edit>: stopping mysql

--- src/requirements/mariadb/tools/init.sh
+++ src/requirements/mariadb/tools/init.sh
@@ -6,8 +6,9 @@ then
 	chmod +x sql_secure_install.exp
 	./sql_secure_install.exp > /dev/null
 	mv /etc/mysql/mariadb.conf.d/50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf.default
-	mv 50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
+	mv /home/50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
 	rm -rf 50-server.cnf
 	service mysql reload
 fi
+service mysql stop
 exec "$@"
