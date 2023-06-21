55a28d92de2704ab3ece6b27e17e9b750a6d33da
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-06-21 09:54:42 +0100

    <update>: new ssl installer

--- src/requirements/nginx/tools/install_ssl.sh
+++ src/requirements/nginx/tools/install_ssl.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-if [ ! -f /etc/ssl/certs/nginx.crt ]; then
-openssl req -newkey rsa:4096 -x509 -nodes -days 365  \
-		-out /etc/ssl/certs/nginx.crt \
-		-keyout /etc/ssl/private/nginx.key \
-		-subj "/C=MA/ST=Khouribga/L=Khouribga/O=1337/OU=Education/CN=sel-kham.42.fr";
+certs_dir="/etc/ssl/certificate/"
+private_dir="/etc/ssl/private/"
+cert_file="${certs_dir}nginx.crt"
+key_file="${private_dir}nginx.key"
+
+mkdir -p "$certs_dir"
+mkdir -p "$private_dir"
+
+if [ ! -f "$cert_file" ]; then
+    openssl req -newkey rsa:4096 -x509 -nodes -days 365 \
+        -out "$cert_file" \
+        -keyout "$key_file" \
+        -subj "/C=MA/ST=Khouribga/L=Khouribga/O=1337/OU=Education/CN=sel-kham.42.fr"
 fi
 nginx -g 'daemon off;';
